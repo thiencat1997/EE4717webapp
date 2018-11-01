5d1d1522ad5a3dc6a889bf19a5c535eb3dd90a62
xm <xingmengpan2013@gmail.com>
2018-11-01 23:55:18 +0800

    register style

--- project/register.php
+++ project/register.php
@@ -3,37 +3,9 @@
     <head>
         <title>User Login/register</title>
         <meta charset="utf-8">
-        <link rel="registerstylesheet" href="registerstyle.css">
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="registerstyle.css">
         <script type = "text/javascript"  src = "form_validation.js" ></script>
-        <!---<style>
-            body {font-family:Verdana, Arial, sans-serif;
-                  background-color: #9ebdef;
-            }
-            #wrapper { background-color: #b3c7e6;
-                       color: #000066;
-                       width: 80%;
-                       margin: auto;
-                       min-width:850px;
-            }
-            #leftcolumn { float: left;
-                          width: 32%;
-                          background-color: white;
-            }
-            #rightcolumn { margin-left: 35%;
-                           background-color: #bfedef;
-                           color: #000000;
-            }
-            .content {padding: 20px 20px 0 20px;
-            }
-            #floatright { margin: 10px;
-                         float: right;
-            }
-            footer {font-size:70%;
-                     text-align: center;
-                     clear: right;
-                     padding-bottom:20px;
-            }
-        </style>--->
     </head>
 <?php
     $servername = "localhost";
@@ -56,7 +28,7 @@
 
 
     <body>
-        <div class="wrapper row">
+        <div class="wrapper">
           <nav>
     				<ul>
     						<li ><a href="index.html" style="text-align:left">
@@ -76,106 +48,82 @@
             <header>
                 <h1>user registeration page</h1>
             </header>
-            <div class=" leftcolumn">
-              <h2>User Login</h2>
+            <main>
+              <div class="login">
+                <h2>User Login</h2>
 
-                <form action="php/login.php" method="POST" id="login_form">
-                  <div class="container ">
-                    <table>
-                      <tr>
-                        <td>Username:</td>
-                        <td><input type="text" name="user" required></input></td>
-                      </tr>
-                      <tr>
-                        <td>Password:</td>
-                        <td><input type="password" name="pwd" required></input></td>
-                      </tr>
-                      <tr>
-                        <td colspan="2">
-                          <span class="pwd">
-                            <a href="#">Forgot password?</a>
-                          </span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td colspan="2">
-                          <span class="uname">
-                            <a href="#">Forgot username?</a>
-                          </span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td colspan="2">
-                          <input type="checkbox" checked="checked" name="remember"> Remember me
-                          </span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td colspan="2" align="right">
-                          <input type="submit" name="login" value="Login" ></input>
-                        </td>
-                      </tr>
-                    </table>
+                  <form action="php/login.php" method="POST" id="login_form">
+                    <div class="container ">
+                        <div>
+                          <label for="user">Username:</label>
+                          <input type="text" name="user" id="user" required></input>
+                        </div>
+                        <div>
+                          <label for="password">Password:</label>
+                          <input type="password" name="password" id="password" required></input>
+                        </div>
+                        <a href="#">Forgot password?</a><br>
+                        <a href="#">Forgot username?</a>
+                        <input type="checkbox" checked="checked" name="remember"> Remember me</input>
+                        <input type="submit" name="login" value="Login" ></input>
+
+                    </div>
+                  </form>
+
+              </div>
+              <div class="registeration">
+                <h2>Registeration</h2>
+                <div class="container">
+                <form action="./php/newuser.php" method="POST" onsubmit="form_validate()" id="register_form">
+                  <div>
+                    <label for="title">Title:</label>
+                    <select name="title" id="title">
+                      <option value=null >--Select--</option>
+                      <option value="Mr">Mr.</option>
+                      <option value="Mrs">Mrs.</option>
+                      <option value="Ms">Ms.</option>
+                      <option value="Miss">Miss.</option>
+                    </select>
                   </div>
-                </form>
+                  <div>
+                    <label for="firstname">*First Name:</label>
+                    <input type="text" id="firstname" name="firstname" placeholder="Your First Name"
+                    id="firstname" required onchange="FirstNameCheck()"></input>
+                  </div>
+                  <div>
+                    <label for="lastname">*Last Name:</label>
+                    <input type="text" id="lastname" name="lastname" id="lastname" required onchange="LastNameCheck()"></input>
+                  </div>
+                  <div>
+                    <label for="username">*Username:</label>
+                    <input type="text" id="username" name="username" required onchange="UsernameCheck()"></input>
+                  </div>
+                  <div>
+                    <label for="pwd">*Password:</label>
+                    <input type="password" id="pwd" name="pwd" required ></input>
+                  </div>
+                  <div>
+                    <label for="cpwd">*Confirm Password:</label>
+                    <input type="password" id="cpwd" name="cpwd" required onchange="PwdCheck()"></input>
+                  </div>
+                  <div>
+                    <label for="email">*Email Address:</label>
+                    <input type="email" id="email" name="email" required></input>
+                  </div>
+                  <div>
+                    <label for="cemail">*Confirm Email:</label>
+                    <input type="email" id="cemail" name="cemail" required onchange="EmailCheck()"></input>
+                  </div>
+                  <div>
+                    <label for="phone">*Phone Number:</label>
+                    <input type="text" id="phone" name="phone" required onchange="PhoneCheck()"></input>
+                  </div>
+                  <button type="submit" onsubmit="ShowAlert()" class="button">Submit</button>
 
-            </div>
-            <div class="rightcolumn">
-              <h2>Registeration</h2>
-              <form action="./php/newuser.php" method="POST" onsubmit="form_validate()" id="register_form">
-                <table>
-                  <tr>
-                    <td>Title:</td>
-                    <td>
-                      <select name="title">
-                        <option value=null >--Select--</option>
-                        <option value="Mr">Mr.</option>
-                        <option value="Mrs">Mrs.</option>
-                        <option value="Ms">Ms.</option>
-                        <option value="Miss">Miss.</option>
-                      </select>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>*First Name:</td>
-                    <td><input type="text" id="firstname" name="firstname" required onchange="FirstNameCheck()"></input></td>
-                  </tr>
-                  <tr>
-                    <td>*Last Name:</td>
-                    <td><input type="text" id="lastname" name="lastname" required onchange="LastNameCheck()"></input></td>
-                  </tr>
-                  <tr>
-                    <td>*Username:</td>
-                    <td><input type="text" id="username" name="username" required onchange="UsernameCheck()"></input></td>
-                  </tr>
-                  <tr>
-                    <td>*Password:</td>
-                    <td><input type="password" id="pwd" name="pwd" required ></input></td>
-                  </tr>
-                  <tr>
-                    <td>*Confirm Password:</td>
-                    <td><input type="password" id="cpwd" name="cpwd" required onchange="PwdCheck()"></input></td>
-                  </tr>
-                  <tr>
-                    <td>*Email Address:</td>
-                    <td><input type="email" id="email" name="email" required></input></td>
-                  </tr>
-                  <tr>
-                    <td>*Confirm Email:</td>
-                    <td><input type="email" id="cemail" name="cemail" required onchange="EmailCheck()"></input></td>
-                  </tr>
-                  <tr>
-                    <td>*Phone Number:</td>
-                    <td><input type="text" id="phone" name="phone" required onchange="PhoneCheck()"></input></td>
-                  </tr>
-                  <tr>
-                    <td colspan="2" align="right">
-                      <input type="submit" name="register" value="register" form="register_form" onsubmit="ShowAlert()"></input>
-                    </td>
-                  </tr>
-                </table>
-              </form>
-            </div>
+                </form>
+              </div>
+              </div>
+            </main>
         </div>
         <script src="form_validation.js"> </script>
     </body>
