c30c10ebf052cf5eceae9bd07552caa872440d55
thiencat1997 <thiencat1997@gmail.com>
2018-11-02 21:50:55 +0800

    login button and password

--- project/register.php
+++ project/register.php
@@ -8,6 +8,7 @@
         <script type = "text/javascript"  src = "form_validation.js" ></script>
     </head>
 <?php
+    session_start();
     $servername = "localhost";
     $username = "f31ee";
     $password = "f31ee";
@@ -28,41 +29,46 @@
 
 
     <body>
-      <div class="banner-container-2" style="background-image: url('media/contact-bg.png');">   
-        <div style="width:1500px; margin: auto" >
-          <?php include_once 'subhtml/navbar.php'; ?>
+      <div class="banner-container-2" style="background-image: url('media/about-bg.png');">   
+        <div style="width:1500px; margin: auto" >    
+         <?php include_once 'subhtml/navbar.php'; ?>
+          <div class="banner-text-2" style="width:100%">
+            Membership Sign In/Up Form
+          </div>
         </div>
       </div>
+      <div style="width:1500px; margin: auto;" >
+          <header>
+              <h1></h1>
+          </header>
+          <main>
+            <div class="login">
+              <h2>User Login</h2>
 
-            <header style="margin-left: 3%;">
-                <h1>Membership Sign In/Up Form</h1>
-            </header>
-            <main>
-              <div class="login">
-                <h2>User Login</h2>
+                <form action="register.php" method="POST" id="login_form">
+                  <div class="container3">
+                      <div>
+                        <label for="user">Username:</label>
+                        <input type="text" name="user" id="user" required ></input>
+                      </div>
+                      <div>
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" id="password" required></input>
+                      </div>
+                      <a href="#">Forgot password?</a><br>
+                      <a href="#">Forgot username?</a><br>
+                      Remember me <input type="checkbox" checked="checked" name="remember" ></input><br>
+                      <button type="submit" onsubmit="ShowAlert()" class="button" style="float: right">Login</button>
+                      <?php
 
-                  <form action="register.php" method="POST" id="login_form">
-                    <div class="container ">
-                        <div>
-                          <label for="user">Username:</label>
-                          <input type="text" name="user" id="user" required></input>
-                        </div>
-                        <div>
-                          <label for="password">Password:</label>
-                          <input type="password" name="password" id="password" required></input>
-                        </div>
-                        <a href="#">Forgot password?</a><br>
-                        <a href="#">Forgot username?</a><br>
-                        Remember me <input type="checkbox" checked="checked" name="remember" ></input><br>
-                        <button type="submit" onsubmit="ShowAlert()" class="button" style="margin-left: 200px;">Login</button>
-                        <?php
-
-                        if (isset($_POST['user']) && isset($_POST['password'])){
-                          $user=$_POST['user'];
-                          $password=$_POST['password'];
-                          $query="select * from users where Username='$user' and Password='$password'";
-                          $run=mysqli_query($conn,$query);
-
+                      if (isset($_POST['user']) && isset($_POST['password'])){
+                        $user=$_POST['user'];
+                        $password=$_POST['password'];
+                        $hash= password_hash( $password , PASSWORD_DEFAULT );
+                        $query="select * from users where Username='$user'";
+                        $run=mysqli_query($conn,$query);
+                        /*
+                        if( password_verify($password, $hashed_password) )
                         if(mysqli_num_rows($run)>0) {
                             echo"<p>login successfully</p>";
                             //echo "<script> window.open('index.php','_self')</script>";
@@ -76,71 +82,91 @@
                             //echo "<script type='text/javascript'>alert('$message');</script>";
                             //header("Location: ../register.php");
                         }
+                        */
+                        if ( mysqli_num_rows($run) > 0) {
+                          // output data of each row
+                          while($row = mysqli_fetch_assoc($run)) {
+                            $hashed_password = $row['Password'];
+                            if( password_verify($password, $hashed_password) ){
+                              echo "<p>login successfully</p>";
+                              $message = "success";
+                              $_SESSION['user']=$user;
+                              echo "I am = $user";
+                              echo $_SESSION['user'];
+                              break;
+                            } else {
+                              echo"<p>invalid password!</p>";
+                            }
+                          }
+                        } else {
+                          echo"<p>invalid username!</p>";
                         }
 
-                        ?>
+                      }
 
-                    </div>
-                  </form>
+                      ?>
 
-              </div>
-              <div class="registeration">
-                <h2>Registeration</h2>
-                <div class="container">
-                <form action="./php/newuser.php" method="POST" onsubmit="form_validate()" id="register_form">
-                  <div>
-                    <label for="title">Title:</label>
-                    <select name="title" id="title">
-                      <option value=null >--Select--</option>
-                      <option value="Mr">Mr.</option>
-                      <option value="Mrs">Mrs.</option>
-                      <option value="Ms">Ms.</option>
-                      <option value="Miss">Miss.</option>
-                    </select>
                   </div>
-                  <div>
-                    <label for="firstname">*First Name:</label>
-                    <input type="text" id="firstname" name="firstname" placeholder="Your First Name"
-                    id="firstname" required onchange="FirstNameCheck()"></input>
-                  </div>
-                  <div>
-                    <label for="lastname">*Last Name:</label>
-                    <input type="text" id="lastname" name="lastname" placeholder="Your Last Name" required onchange="LastNameCheck()"></input>
-                  </div>
-                  <div>
-                    <label for="username">*Username:</label>
-                    <input type="text" id="username" name="username" placeholder="Choose a Username" required onchange="UsernameCheck()"></input>
-                  </div>
-                  <div>
-                    <label for="pwd">*Password:</label>
-                    <input type="password" id="pwd" name="pwd" placeholder="Choose a password"required ></input>
-                  </div>
-                  <div>
-                    <label for="cpwd">*Confirm Password:</label>
-                    <input type="password" id="cpwd" name="cpwd" placeholder="Confirm password" required onchange="PwdCheck()"></input>
-                  </div>
-                  <div>
-                    <label for="email">*Email Address:</label>
-                    <input type="email" id="email" name="email" placeholder="Your Email" required></input>
-                  </div>
-                  <div>
-                    <label for="cemail">*Confirm Email:</label>
-                    <input type="email" id="cemail" name="cemail" placeholder="Confirm Email" required onchange="EmailCheck()"></input>
-                  </div>
-                  <div>
-                    <label for="phone">*Phone Number:</label>
-                    <input type="text" id="phone" name="phone" placeholder="Your Phone Number" required onchange="PhoneCheck()"></input>
-                  </div>
-                  <button type="submit" onsubmit="ShowAlert()" class="button" style="margin-left: 270px;">Submit</button>
-
                 </form>
-              </div>
-              </div>
-            </main>
+            </div>
+            <div class="registeration">
+              <h2>Registeration</h2>
+              <div class="container3">
+              <form action="./php/newuser.php" method="POST" onsubmit="form_validate()" id="register_form">
+                <div>
+                  <label for="title">Title:</label>
+                  <select name="title" id="title">
+                    <option value=null >--Select--</option>
+                    <option value="Mr">Mr.</option>
+                    <option value="Mrs">Mrs.</option>
+                    <option value="Ms">Ms.</option>
+                    <option value="Miss">Miss.</option>
+                  </select>
+                </div>
+                <div>
+                  <label for="firstname">*First Name:</label>
+                  <input type="text" id="firstname" name="firstname" placeholder="Your First Name"
+                  id="firstname" required onchange="FirstNameCheck()"></input>
+                </div>
+                <div>
+                  <label for="lastname">*Last Name:</label>
+                  <input type="text" id="lastname" name="lastname" placeholder="Your Last Name" required onchange="LastNameCheck()"></input>
+                </div>
+                <div>
+                  <label for="username">*Username:</label>
+                  <input type="text" id="username" name="username" placeholder="Choose a Username" required onchange="UsernameCheck()"></input>
+                </div>
+                <div>
+                  <label for="pwd">*Password:</label>
+                  <input type="password" id="pwd" name="pwd" placeholder="Choose a password"required ></input>
+                </div>
+                <div>
+                  <label for="cpwd">*Confirm Password:</label>
+                  <input type="password" id="cpwd" name="cpwd" placeholder="Confirm password" required onchange="PwdCheck()"></input>
+                </div>
+                <div>
+                  <label for="email">*Email Address:</label>
+                  <input type="email" id="email" name="email" placeholder="Your Email" required></input>
+                </div>
+                <div>
+                  <label for="cemail">*Confirm Email:</label>
+                  <input type="email" id="cemail" name="cemail" placeholder="Confirm Email" required onchange="EmailCheck()"></input>
+                </div>
+                <div>
+                  <label for="phone">*Phone Number:</label>
+                  <input type="text" id="phone" name="phone" placeholder="Your Phone Number" required onchange="PhoneCheck()"></input>
+                </div>
+                <br>
+                <button type="submit" onsubmit="ShowAlert()" class="button" style="float: right">Submit</button>
+
+              </form>
+            </div>
+          </main>
+      </div>
 
         <script src="form_validation.js"> </script>
 
-        <?php include_once 'footer.php'; ?>
+        <?php include_once 'subhtml/footer.php'; ?>
 
     </body>
 </html>
--- project/subhtml/navbar.php
+++ project/subhtml/navbar.php
@@ -1,9 +1,33 @@
+<link rel="stylesheet" href="styles.css">
+
+<?php
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    }
+	if(!empty($_SESSION['user'])){
+		$link = "status.php";
+		$text = "Hi, ".$_SESSION['user']."!";
+
+	} else{			
+		$link = "register.php";
+		$text = "Login";
+	}
+?>
+
+
 <nav>
 	<ul>
 			<li ><a href="index.php" style="text-align:left">
 				<img src="media/logo.png" height="100%" width= "100%">
 			</a> </li>
-		  	<li style="float:right"><a class="button" href="register.php" style="padding: 15px; width: 150px">Login</a></li>
+		  	<li style="float:right">
+
+
+  <a class='button' href=' <?php echo $link ?> ' style='padding: 15px; width: 300px'><?php echo $text ?> </a>
+
+
+		  	</li>
 
 		<strong>
 			<li style="float:right">	<a href="appointment.php">Appointment</a> </li>
