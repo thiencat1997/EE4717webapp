b28f75d0cde526237f2d644057b4039e879eb54f
xm <xingmengpan2013@gmail.com>
2018-11-03 16:37:43 +0800

    disable appointment when session_user is null

--- project/subhtml/navbar.php
+++ project/subhtml/navbar.php
@@ -1,18 +1,22 @@
 <link rel="stylesheet" href="styles.css">
 
 <?php
-    if(!isset($_SESSION)) 
-    { 
-        session_start(); 
+    if(!isset($_SESSION))
+    {
+        session_start();
     }
 	if(!empty($_SESSION['user'])){
 		$link = "status.php";
 		$text = "Hi, ".$_SESSION['user']."!";
 
-	} else{			
+	} else{
 		$link = "register.php";
 		$text = "Login";
 	}
+  function phpAlert() {
+    echo '<script type="text/javascript">alert("Please login first!")</script>';
+  }
+
 ?>
 
 
@@ -30,7 +34,18 @@
 		  	</li>
 
 		<strong>
-			<li style="float:right">	<a href="booking.php" style='width: 300px'>Book Appointment</a> </li>
+			<li style="float:right">
+        <?php
+        if(empty($_SESSION['user'])){
+          phpAlert();
+          echo '<a href="javascript:void(0);">Book Appointment</a>';
+        }else{
+          echo '<a href="booking.php">Book Appointment</a>';}
+        ?>
+
+
+
+      </li>
 			<li style="float:right">	<a href="service.php">Service</a> </li>
 			<li style="float:right">	<a href="about.php">About</a> </li>
 			<li style="float:right">	<a href="index.php">Home</a> </li>
