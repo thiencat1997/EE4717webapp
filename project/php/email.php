8e42d51290dd2e8ca93b8b105dfdede8cc91c85c
#LE MAU THIEN CAT# <CAT001@student.main.ntu.edu.sg>
2018-11-07 17:15:42 +0800

    email

--- project/php/email.php
+++ project/php/email.php
@@ -9,9 +9,8 @@
     $email_from = 'f31ee@localhost';
     $message='hello from php mail';
     $email_subject = 'New Appointment Confirmation - MedArt Clinic';
-    $email_body = "Dear $name,\n You have successfully made an appointment to MedArt Clinic.".
-                            "Here is the details:\n $message". "Feel free to modify it anytime at our website: http://localhost/EE4717webapp/project/status.php".;
+    //$email_body = "Dear $name,\n You have successfully made an appointment to MedArt Clinic."."Here is the details:\n $message". "Feel free to modify it anytime at our website: http://localhost/EE4717webapp/project/status.php".;
     $headers = 'From: $email_from'. "\r\n".'Reply-To: $user_email'. "\r\n".'X-Mailer: PHP/'.phpversion();
-    mail($to,$email_subject,$email_body,$headers,'-f31ee@localhost');
+    mail($to,$email_subject,$message,$headers,'ff31ee@localhost');
     echo ('mail sent to :'.$to);
 ?>
--- project/php/submitbook.php
+++ project/php/submitbook.php
@@ -22,5 +22,18 @@
     $query = "INSERT INTO booking (UserID, Service, DrName, BookDate,BookTime, SlotID, Remarks) VALUES ('$user','$service', '$doctor', '$date', '$time','$slotid[0]', '$remarks')";
     $result = mysqli_query($conn, $query);
     $_SESSION['updated'] = true;
+	
+	$name=$_SESSION['user'];
+    $to = 'f31ee@localhost';
+    $email_from = 'f31ee@localhost';
+    $message='hello from php mail';
+    $email_subject = 'New Appointment Confirmation - MedArt Clinic';
+    //$email_body = "Dear $name,\n You have successfully made an appointment to MedArt Clinic."."Here is the details:\n $message". "Feel free to modify it anytime at our website: http://localhost/EE4717webapp/project/status.php".;
+    $headers = 'From: $email_from'. "\r\n".'Reply-To: $user_email'. "\r\n".'X-Mailer: PHP/'.phpversion();
+    mail($to,$email_subject,$message,$headers,'ff31ee@localhost');
+    echo ('mail sent to :'.$to);
+	
     header('location: ../booking.php');
+	
+	
 ?>
