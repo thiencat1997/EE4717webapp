272a9ae2d1603ce3328497d2a053734e61a67de6
xm <xingmengpan2013@gmail.com>
2018-11-07 17:06:31 +0800

    email testing

--- project/php/email.php
+++ project/php/email.php
@@ -1,13 +1,17 @@
 <?php
   include_once 'db_connect.php';
-
-    $email_from = 'xingmengpan2013@gmail.com';
-    $email_subject = "New Appointment Confirmation - MedArt Clinic";
+  if(!isset($_SESSION))
+  {
+      session_start();
+  }
+  $name=$_SESSION['user'];
+    $to = 'f31ee@localhost';
+    $email_from = 'f31ee@localhost';
+    $message='hello from php mail';
+    $email_subject = 'New Appointment Confirmation - MedArt Clinic';
     $email_body = "Dear $name,\n You have successfully made an appointment to MedArt Clinic.".
                             "Here is the details:\n $message". "Feel free to modify it anytime at our website: http://localhost/EE4717webapp/project/status.php".;
-    $to = "$user_email";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $user_email \r\n";
-    mail($to,$email_subject,$email_body,$headers);
-
+    $headers = 'From: $email_from'. "\r\n".'Reply-To: $user_email'. "\r\n".'X-Mailer: PHP/'.phpversion();
+    mail($to,$email_subject,$email_body,$headers,'-f31ee@localhost');
+    echo ('mail sent to :'.$to);
 ?>
